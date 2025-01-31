cbd994d7f5c83ed346d7ad86c86f650df5cab9b2
Marina <marinabrito.ma@gmail.com>
2025-01-31 19:24:51 +0000

    added methods get by emailand by period

--- src/main/java/com/javanauta/taskscheduler/business/TaskService.java
+++ src/main/java/com/javanauta/taskscheduler/business/TaskService.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -19,8 +20,8 @@ public class TaskService {
     private final TaskConverter taskConverter;
     private final JwtUtil jwtUtil;
 
-    public TaskDTO saveTask(String token, TaskDTO taskDTO){
-        String email =  jwtUtil.extrairEmailToken(token.substring(7));
+    public TaskDTO saveTask(String token, TaskDTO taskDTO) {
+        String email = jwtUtil.extrairEmailToken(token.substring(7));
 
         taskDTO.setDateCreated(LocalDateTime.now());
         taskDTO.setNotificationStatusEnum(NotificationStatusEnum.PENDENT);
@@ -29,5 +30,21 @@ public class TaskService {
         TaskEntity taskEntity = taskConverter.toTaskEntity(taskDTO);
 
         return taskConverter.toTaskDTO(taskRepository.save(taskEntity));
+    }
+
+    public List<TaskDTO> findTaskSchedulerByPeriod(LocalDateTime startDate,
+                                                   LocalDateTime endDate,
+                                                   NotificationStatusEnum statusEnum) {
+        return taskConverter.toListTaskDTO(
+                taskRepository.findByDateEventBetweenAndNotificationStatusEnum(
+                        startDate, endDate, statusEnum
+                ));
+    }
+
+    public List<TaskDTO> findTasksByEmail(String token){
+        String email = jwtUtil.extrairEmailToken(token.substring(7));
+        List<TaskEntity> taskEntityList = taskRepository.findByUserEmail(email);
+
+        return taskConverter.toListTaskDTO(taskEntityList);
     }
 }
--- src/main/java/com/javanauta/taskscheduler/controller/TaskController.java
+++ src/main/java/com/javanauta/taskscheduler/controller/TaskController.java
@@ -2,10 +2,15 @@ package com.javanauta.taskscheduler.controller;
 
 import com.javanauta.taskscheduler.business.TaskService;
 import com.javanauta.taskscheduler.business.dto.TaskDTO;
+import com.javanauta.taskscheduler.insfrastruture.enums.NotificationStatusEnum;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/task")
@@ -20,4 +25,25 @@ public class TaskController {
                                                 @RequestHeader("Authorization") String token) {
         return ResponseEntity.ok(taskService.saveTask(token, taskDTO));
     }
+
+    @GetMapping("/events")
+    public ResponseEntity<List<TaskDTO>> findListTaskByPeriod(
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)LocalDateTime dateCreated,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)LocalDateTime dateUpdated,
+            @RequestParam NotificationStatusEnum statusEnum
+    ){
+
+        return ResponseEntity.ok(taskService.findTaskSchedulerByPeriod(
+                dateCreated, dateUpdated, statusEnum
+        ));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TaskDTO>> findListByEmail(
+            @RequestHeader("Authorization") String token){
+        List<TaskDTO> tasks = taskService.findTasksByEmail(token);
+
+        return ResponseEntity.ok(tasks);
+    }
+
 }
